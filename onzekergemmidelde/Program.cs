f0bcf2d7893405bb071c08837c8ab412f4ac396c
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-11-26 09:54:08 +0100

    4de commit

--- onzekergemmidelde/Program.cs
+++ onzekergemmidelde/Program.cs
@@ -24,22 +24,36 @@ namespace onzekergemmidelde
     {
         static void Main(string[] args)
         {   //DECLARATIES
-            int geheelgetalen, gemiddelde;
+            int geheelgetalen1, geheelgetal2,geheelgetal3, geheelgetal4, geheelgetal5 , aantalgetalen, gemiddelde;
 
 
 
             // VRAAG een aantal gehele getallen
             Console.WriteLine("geef een aantal gehele getallen!: ");
-            geheelgetalen = int.Parse(Console.ReadLine());
+            geheelgetalen1 = int.Parse(Console.ReadLine());
 
-            
+            Console.WriteLine("geef een aantal gehele getallen!: ");
+            geheelgetal2= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("geef een aantal gehele getallen!: ");
+            geheelgetal3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("geef een aantal gehele getallen!: ");
+            geheelgetal4 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("geef een aantal gehele getallen!: ");
+            geheelgetal5 = int.Parse(Console.ReadLine());
+
+
+
+
             //BEREKEN  GEMMIDDELDE(tel al de gegeven getallen op)
-             
+            gemiddelde = geheelgetalen / aantalgetalen;
             //(deel de opgetelde getallen door het aantal ingegeven getallen)
             //(resultaat mac 3 getal na de komma en min 1 na de komma)
 
             //TOON gemmidelde van ingegeven getallen max 3 na de komma min 1 na de komma
-
+            Console.WriteLine(gemiddelde);
             //WACHTEN
             Console.WriteLine();
             Console.WriteLine("druk op enter om af te sluiten");
